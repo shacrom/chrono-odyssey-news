eb711263a89bfbd42dc22a4adc98be6ba33e0138
Marcos Miguel <shacrommb@gmail.com>
2025-09-04 20:35:56 +0200

    fix: add @astrojs/node dependency to package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shacrom.com',
   output: 'server', // Modo servidor para todas las páginas
+  adapter: node({mode: 'standalone'}),
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
